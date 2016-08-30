d35c8e9a6444b837d7190ad41ad91c46d6ee0a7b
Kevin <kevin.jiang@lexisnexis.com>
2016-08-30 18:45:17 +0800

    first commit

---
+++ init.el
@@ -0,0 +1,123 @@
+;; author: Kevin.Jiang
+;; E-mail: kittymiky@gmail.com
+
+;; 设置我的个人信息
+(setq user-full-name "Kevin Jiang")
+(setq user-mail-address "kittymiky@gmail.com")
+
+
+;; 关闭启动画面
+(setq inhibit-startup-message t)
+
+;; 在标题栏提示你目前在什么位置
+(defun frame-title-string ()
+  "Return the file name of current buffer, using ~ if under home directory"
+  (let ((fname (or
+               (buffer-file-name (current-buffer))
+               (buffer-name)))
+       (max-len 100))
+    (when (string-match (getenv "HOME") fname)
+      (setq fname (replace-match "~" t t fname)))
+    (if (> (length fname) max-len)
+        (setq fname
+              (concat "..."
+                      (substring fname (- (length fname) max-len)))))
+    fname))
+(setq frame-title-format '("Kevin@"(:eval (frame-title-string))))
+
+;; 去掉工具栏
+(tool-bar-mode 0)
+
+;; 显示时间，格式如下
+(display-time-mode 1)
+(setq display-time-24hr-format t)
+(setq display-time-day-and-date t)
+(transient-mark-mode t)
+
+;; 在status bar显示行号和列号
+(line-number-mode)
+(column-number-mode)
+
+;; 显示行列号
+(global-linum-mode t)
+
+;; 高亮当前行
+(global-hl-line-mode t)
+
+;; 当emacs退出时保存打开的文件
+(desktop-save-mode 1)
+(setq-default desktop-load-locked-desktop t)
+(desktop-load-default)
+(desktop-read)
+
+;; 记住上次文件打开的位置
+(setq-default save-place t)
+
+;; 当emacs退出时保存文件打开状态
+(add-hook 'kill-emacs-hook
+	  '(lambda()
+	     (desktop-save "~/.emacs.d")))
+
+;; 不产生备份文件
+(setq make-backup-files nil)
+
+;; 以 y/n 代表 yes/no
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;; 显示括号匹配,但不跳转到另外一个括号的位置
+(show-paren-mode t)
+;; 开启括号自动补全
+(electric-pair-mode t)
+
+;; 把kill-ring设置大一些
+(setq-default kill-ring-max 100000)
+
+
+;;; 设置load-path
+(add-to-list 'load-path "~/.emacs.d/lisp/")
+
+
+;;; install packages
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+
+(setq package-list
+      '(helm
+	auto-complete
+	yasnippet
+	company
+	magit))
+
+(unless package-archive-contents
+  (package-refresh-contents))
+
+(dolist (pkg package-list)
+  (unless (package-installed-p pkg)
+    (package-install pkg)))
+
+
+;;; helm
+(require 'helm-config)
+(helm-mode 1)
+
+
+;;; auto complete
+(ac-config-default)
+(setq ac-sources
+      '(ac-source-filename
+	ac-source-functions
+	ac-source-yasnippet
+	ac-source-variables
+	ac-source-symbols
+	ac-source-features
+	ac-source-abbrev
+	ac-source-words-in-same-mode-buffers
+	ac-source-dictionary))
+(global-auto-complete-mode t)
+(yas-global-mode 1)
+(add-hook 'after-init-hook 'global-company-mode)
+
+
+(load "my-key-bindings")
---
+++ my-key-bindings.el
@@ -0,0 +1,6 @@
+
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
+
+(global-set-key (kbd "M-x") 'helm-M-x)
+
+(global-set-key (kbd "C-c h o") 'helm-occur)
