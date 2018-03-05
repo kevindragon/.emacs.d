d50b2eca68314485f4235d4babfa825f748e729f
Kevin Jiang <kittymiky@gmail.com>
2018-03-05 16:41:34 +0800

    Update init.el

--- init.el
+++ init.el
@@ -271,7 +271,9 @@ If the new path's directories does not exist, create them."
   (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
   (setq web-mode-markup-indent-offset 2)
-  (setq web-mode-css-indent-offset 2))
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2)
+  (setq web-mode-enable-current-column-highlight t))
 
 (when (package-installed-p 'window-numbering)
   (window-numbering-mode))
