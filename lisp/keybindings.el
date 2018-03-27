833b905aa4d89b65c73eb6d3f207f0624c1f1cfa
Kevin Jiang <kittymiky@gmail.com>
2018-03-27 13:40:22 +0800

    Update keybindings.el

--- lisp/keybindings.el
+++ lisp/keybindings.el
@@ -15,15 +15,15 @@
                   (describe-bindings)
                   (other-window 1)))
 
+(global-set-key (kbd "C-S-o")
+                (lambda ()
+                  (interactive)
+                  (move-end-of-line 1)
+                  (newline)
+                  (indent-for-tab-command)))
+
 ;; magit key bindings
 (global-set-key (kbd "C-x g") 'magit-status)
-
-;; fold
-(global-set-key (kbd "C-c z c") 'vimish-fold)
-(global-set-key (kbd "C-c z o") 'vimish-fold-unfold)
-
-(global-set-key "\M-\C-g" 'org-plot/gnuplot)
-
 
 ;;; window operations
 ;; window split horizontal and vertical toggle
